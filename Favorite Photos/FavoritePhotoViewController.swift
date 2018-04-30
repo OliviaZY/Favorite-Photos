7b475bbe28b06b04a1cb5fc26920183845811214
Yuan zhou <zhouy40630@gmail.com>
2018-04-30 12:11:20 -0400

    add camera to take a picture

--- Favorite Photos/FavoritePhotoViewController.swift
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -18,9 +18,41 @@ class FavoritePhotoViewController: UIViewController {
         
     }
     @IBAction func takePhoto(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            imagePicker.sourceType = .camera
+        }else{
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated:true)
+    }
+}
+
+// mark: UIImagepicker controller delegate methods
+extension FavoritePhotoViewController: UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
     }
     
-
-    
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            //            TODO: upload the data to storage, display after the storaeg save is done
+            self.iamgeview.image = image
+        }
+        picker.dismiss(animated: true)
+    }
 }
+
+
+
+
+
+
+
+
+
+
