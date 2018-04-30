316860de3111f8660ae940c5672dc2e0810c0782
Yuan zhou <zhouy40630@gmail.com>
2018-04-30 11:58:19 -0400

    user interface setup

---
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FavoritePhotoViewController.swift
+//  Favorite Photos
+//
+//  Created by Yuan zhou on 4/30/18.
+//  Copyright © 2018 Yuan zhou. All rights reserved.
+//
+
+import UIKit
+
+class FavoritePhotoViewController: UIViewController {
+
+    @IBOutlet weak var iamgeview: UIImageView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func takePhoto(_ sender: Any) {
+    }
+    
+
+    
+
+}
